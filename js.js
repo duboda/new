de754715da045492df647afa6c6ffe000df3fc6d
duboda <duboda507@gmail.com>
2014-04-12 23:40:55 -0500

    new

--- js.js
+++ js.js
@@ -2,13 +2,12 @@ function myFunction()
 {
 Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhWwGtM9niEiDcSKs4mOKSNbp");
       var TechBathroom = Parse.Object.extend("TechBathroom");
-      var techbathroom = new TechBathroom();
-      techbathroom.save({gender: "M", floor: 0, latitude: 42.05722, longitude: 87.6752778, stallnumber: 3 }).then(function(object) {
+      var techbathroom1 = new TechBathroom();
+      techbathroom1.save({gender: "M", floor: 0, latitude: 42.05722, longitude: 87.6752778, stallnumber: 3 }).then(function(object) {
         alert("yay! it worked");
       });
-      var TechBathroom = Parse.Object.extend("TechBathroom");
-      var techbathroom = new TechBathroom();
-      techbathroom.save({gender: "M", floor: 0, latitude: 42.05750, longitude: 87.6755556, stallnumber: 3 }).then(function(object) {
+      var techbathroom2 = new TechBathroom();
+      techbathroom2.save({gender: "M", floor: 0, latitude: 42.05750, longitude: 87.6755556, stallnumber: 3 }).then(function(object) {
         alert("yay! it worked");
       });      
 };
