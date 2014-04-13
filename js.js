d2c8c72dbf815ed8699cc6d0fe91d96f84ec9c7d
duboda <duboda507@gmail.com>
2014-04-12 23:36:13 -0500

    new

--- js.js
+++ js.js
@@ -6,6 +6,8 @@ Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhW
       techbathroom.save({gender: "M", floor: 0, latitude: 42.05722, longitude: 87.6752778, stallnumber: 3 }).then(function(object) {
         alert("yay! it worked");
       });
+      var TechBathroom = Parse.Object.extend("TechBathroom");
+      var techbathroom = new TechBathroom();
       techbathroom.save({gender: "M", floor: 0, latitude: 42.05750, longitude: 87.6755556, stallnumber: 3 }).then(function(object) {
         alert("yay! it worked");
       });      
