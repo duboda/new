4999cbd8b1d6c3b5ed0f77ddc0f4656acd044ec0
duboda <duboda507@gmail.com>
2014-04-14 20:27:50 -0500

    a

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ function myFunctionSave()
 Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhWwGtM9niEiDcSKs4mOKSNbp");
       var TechBathroom = Parse.Object.extend("TechBathroom");
       var techbathroom1 = new TechBathroom();
-      techbathroom1.save({gender: "F", floor: 0, latitude: 42.05722, longitude: -87.6752778, stallnumber: 3});
+      techbathroom1.save({gender: "F", floor: 0, latitude: 42.05722, longitude: -87.6752778, stallnumber: 3, name: "G01" });
       var techbathroom2 = new TechBathroom();
       techbathroom2.save({gender: "F", floor: 0, latitude: 42.05750, longitude: -87.6755556, stallnumber: 3, name: "G02" });
       var techbathroom3 = new TechBathroom();
