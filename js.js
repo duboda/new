6f6a55f1a047fc6f4b7b0368e3cf56d4dc0e68ad
duboda <duboda507@gmail.com>
2014-05-10 13:48:27 -0500

    a

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 function myFunctionSave()
 {
 Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhWwGtM9niEiDcSKs4mOKSNbp");
+
       var TechBathroom = Parse.Object.extend("TechBathroom");
       var techbathroom1 = new TechBathroom();
       techbathroom1.save({gender: "F", floor: 0, latitude: 42.05722, longitude: -87.6752778, stallnumber: 3, name: "G01", cleanliness: 0, spaciousness: 0, odor: 0, waittime: 0, overall: 0, number: 0 });
@@ -124,9 +125,10 @@ function myFunctionQuery(){
 function myFunctionFran()
 {
 Parse.initialize("om9ynedsIy67rU9vfQh8IVR2vv0A6WnFz0jgWUrP", "mzPU7M8YQwD83alRhWwGtM9niEiDcSKs4mOKSNbp");
+      var array[10];
       var TechBathroom = Parse.Object.extend("FranBathroom");
       var techbathroom5 = new TechBathroom();
-      techbathroom5.save({gender: "F", floor: 1, latitude: 42.0588889, longitude: -87.673888889, stallnumber: 1, name: "f107", cleanliness: 0, spaciousness: 0, odor: 0, waittime: 0, overall: 0, number: 0  });
+      techbathroom5.save({gender: "F", floor: array[], latitude: 42.0588889, longitude: -87.673888889, stallnumber: 1, name: "f107", cleanliness: 0, spaciousness: 0, odor: 0, waittime: 0, overall: 0, number: 0  });
       var techbathroom6 = new TechBathroom();
       techbathroom6.save({gender: "M", floor: 1, latitude: 42.0588889, longitude: -87.673888889, stallnumber: 2, name: "f108", cleanliness: 0, spaciousness: 0, odor: 0, waittime: 0, overall: 0, number: 0   });     
       var techbathroom7 = new TechBathroom();
